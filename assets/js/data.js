ee6dd12b843a281c9a7fbc5baaaca1241eada1da
unknown <verma199727@gmail.com>
2023-02-13 15:54:20 +0530

    test

---
+++ assets/js/data.js
@@ -0,0 +1,75 @@
+﻿const MASTER_DB = {
+    "CONFIG": {
+        "MAX_QUES_PER_SCREEN": 1,
+        "TITLE": "Choose the correct options",
+    },
+
+    "QUESTIONS": [
+        {
+            "title": "She is baking in the kitchen",
+            "options": {
+                "She": ["1"],
+                "in": ["4"],
+                "is": ["2"],
+                "the": ["5"],
+                "baking": ["3"],
+                "kitchen": ["6"],
+            },
+        },
+        {
+            "title": "",
+            "options": {
+                "She": ["1"],
+                "in": ["4"],
+                "is": ["2"],
+                "the": ["5"],
+                "baking": ["3"],
+            },
+        },
+        {
+            "title": "",
+            "options": {
+                "She": ["1"],
+                "in": ["4"],
+                "is": ["2"],
+                "the": ["5"],
+                "baking": ["3"],
+                "kitchen": ["6"],
+            },
+        },
+        {
+            "title": "",
+            "options": {
+                "She": ["1"],
+                "in": ["4"],
+                "is": ["2"],
+                "the": ["5"],
+                "baking": ["3"],
+            },
+        },
+        {
+            "title": "",
+            "options": {
+                "She": ["1"],
+                "in": ["4"],
+                "is": ["2"],
+                "the": ["5"],
+                "baking": ["3"],
+            },
+        },
+        {
+            "title": "",
+            "options": {
+                "She": ["1"],
+                "in": ["4"],
+                "is": ["2"],
+                "the": ["5"],
+                "baking": ["3"],
+                "kitchen": ["6"],
+            },
+        },
+        {
+            "title": "Well Done",
+        },
+    ]
+}
